84aa4b5963bd393b34148bb4c1c417415e22c1c4
James Maguire <jaindreas@gmail.com>
2021-03-19 01:47:21 +0000

    First Commit

---
+++ script.js
@@ -0,0 +1,39 @@
+function fetchPage1() {
+    return fetch('https://www.strava.com/api/v3/clubs/817965/activities?page=1&per_page=200&access_token=f733fff0554468ec1b3dc330713cedd9ba85b82b')
+        .then(response => response.json())
+        .then((json) => {
+            let count = 0
+            for (let ele of json) {
+                count += ele.distance
+            }
+            let recordPageNum = json.length
+            return [count, recordPageNum]
+        })
+}
+
+function fetchPage2() {
+    return fetch('https://www.strava.com/api/v3/clubs/817965/activities?page=2&per_page=200&access_token=f733fff0554468ec1b3dc330713cedd9ba85b82b')
+        .then(response => response.json())
+        .then((json) => {
+            let count = 0
+            for (let ele of json) {
+                count += ele.distance
+            }
+            let recordPageNum = json.length
+            return [count, recordPageNum]
+        })
+}
+
+async function getData() {
+    let valuesP1 = await fetchPage1()
+    let valuesP2 = await fetchPage2()
+    let countP1 = valuesP1[0]
+    let countP2 = valuesP2[0]
+    let total = countP1 + countP2
+    let recordTotal = valuesP1[1] + valuesP2[1]
+    document.getElementById("data").innerText = `${Math.floor(total * 0.000621)} Miles (${Math.floor(total / 1000)} Kilometers)`
+    document.getElementById("recordCount").innerText = `Club Activities: ${recordTotal}`
+}
+
+    getData();
+   
